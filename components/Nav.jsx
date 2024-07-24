4f51e7182b37eecb8faa801521bdadcb43fa0193
Prerna-Kujur <131394177+Prerna-Kujur@users.noreply.github.com>
2024-07-24 17:25:03 +0530

    Update Nav.jsx
    Signed-off-by: Prerna-Kujur <131394177+Prerna-Kujur@users.noreply.github.com>

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -29,7 +29,7 @@ const Nav = () => {
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href="/" className='flex gap-2 flex-center'>
         <Image src="/assets/images/logo.svg" alt="promptopia Logo" width={30} height={30} className='object-contain' />
-        <p className='logo_text'>Promptopia</p>
+        <p className='logo_text'>PromptHUB</p>
       </Link>
 
       {/* desktop navigation*/}

d95cecc87c96e6d7346f1310decf659e7eb19a78
Prerna-Kujur <131394177+Prerna-Kujur@users.noreply.github.com>
2024-07-24 15:05:28 +0530

    Update layout.jsx
    Signed-off-by: Prerna-Kujur <131394177+Prerna-Kujur@users.noreply.github.com>

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Nav from '@components/Nav';
 import { Provider } from '@components/Provider';
 
 export const metadata={
-    title:"promptopia",
+    title:"promptopHUB",
     description:'Discover & Share AI Prompts'
 }
 
